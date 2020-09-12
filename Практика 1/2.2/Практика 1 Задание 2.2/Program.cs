36d2c78732611455279c2dbf69eaef2d0d5b53e9
NickVest <47671600+NickVest@users.noreply.github.com>
2020-09-12 11:27:34 +0300

    Add files via upload

---
+++ Практика 1/2.2/Практика 1 Задание 2.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Практика_1_Задание_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+      string[] sa = s.Split(" ");
+
+      Array.Sort(sa);
+
+      s = string.Join(" ", sa);
+      Console.WriteLine(s);
+    }
+  }
+}
