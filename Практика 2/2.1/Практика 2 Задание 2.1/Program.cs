52ffc4241f050e5b62c53d25fe6fd22e611462d1
NickVest <47671600+NickVest@users.noreply.github.com>
2020-09-20 16:16:09 +0300

    Add files via upload

---
+++ Практика 2/2.1/Практика 2 Задание 2.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Практика_2_Задание_2._1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			StreamWriter writer = File.CreateText(@"newfile.txt");
+			writer.WriteLine("This is my new file");
+			writer.WriteLine("Do you like its format?"); writer.Close();
+
+			StreamReader reader = File.OpenText(@"newfile.txt");
+			string contents = reader.ReadToEnd();
+			reader.Close();
+			Console.WriteLine(contents);
+		}
+	}
+}
