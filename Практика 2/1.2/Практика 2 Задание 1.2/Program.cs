0c671f6ebbefbf87665d9556f06879c6e8e28a56
NickVest <47671600+NickVest@users.noreply.github.com>
2020-09-20 18:51:44 +0300

    Add files via upload

--- Практика 2/1.2/Практика 2 Задание 1.2/Program.cs
+++ Практика 2/1.2/Практика 2 Задание 1.2/Program.cs
@@ -5,21 +5,20 @@ namespace Практика_2_Задание_1._2
 {
 	class Program
 	{
-		public static bool EnabhngRaisingEvents { get; private set; }
-
+		static void watcher_Changed(object sender, FileSystemEventArgs e)
+		{
+			Console.WriteLine("Changed: {0}", e.FullPath);
+		}
 		static void Main(string[] args)
 		{
 			FileSystemWatcher watcher = new FileSystemWatcher(Environment.SystemDirectory);
 			watcher.Filter = "*.ini";
 			watcher.IncludeSubdirectories = true;
 			watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
-			watcher.Changed += new FileSystemEventHandler(watcher.Changed);
+			watcher.EnableRaisingEvents = true;
+			watcher.Changed += new FileSystemEventHandler(watcher_Changed);
 		}
 
-		static void watcher_Changed(object sender, FileSystemEventArgs e)
-		{
-			Console.WriteLine("Changed: {0}", e.FullPath);
-			EnabhngRaisingEvents = true;
-		}
+		
 	}
 }
