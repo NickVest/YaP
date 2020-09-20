60e8acdb13c9101e87f79de337b159d840db37e8
NickVest <47671600+NickVest@users.noreply.github.com>
2020-09-20 15:42:28 +0300

    Add files via upload

---
+++ Практика 2/1.1/Практика 2 Задание 1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Практика_2_Задание_1._1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory); ShowDirectory(dir);
+		}
+		static void ShowDirectory(DirectoryInfo dir)
+		{
+			// Показать все файлы
+			foreach (FileInfo file in dir.GetFiles())
+			{
+				Console.WriteLine("File: {0}", file.FullName);
+			}
+			foreach (DirectoryInfo subDir in dir.GetDirectories())
+			{
+				ShowDirectory(subDir);
+			}
+		}
+	}
+}
