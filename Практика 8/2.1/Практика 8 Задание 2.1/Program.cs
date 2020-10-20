ceb4c56eb5f51e8ec9eeba8ecd8b69c1f5cab9e8
NickVest <47671600+NickVest@users.noreply.github.com>
2020-10-20 15:48:16 +0300

    Add files via upload

---
+++ Практика 8/2.1/Практика 8 Задание 2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Практика_8_Задание_2.1
+{
+  class Program
+{
+  static void Main(string args)
+  {
+    Mutex oneMutex = null;
+    const string MutexName = "RUNMEONLYONCE";
+    try // ПытаемсяоткрытьMutex
+    {
+      oneMutex = Mutex.OpenExisting(MutexName);
+    }
+    catch (WaitHandleCannotBeOpenedException)
+    {
+      // He можемоткрытьMutex, потомучтооннесуществует
+    }
+    // Создаем его, если он не существует 
+    if (oneMutex == null)
+    {
+      oneMutex = new Mutex(true, MutexName);
+    }
+    else
+    {
+      // Закрываем Mutexи выходим из приложения,
+      // так как разрешается запуск только одного его экземпляра
+      oneMutex.Close();
+      return;
+    }
+    Console.WriteLine("Our Application");
+    Console.Read();
+  }
+}
+}
