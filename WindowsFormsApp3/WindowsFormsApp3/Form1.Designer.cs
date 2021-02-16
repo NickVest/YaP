277be11c69da5bc9040a6978941e662a1aa6cbad
NickVest <47671600+NickVest@users.noreply.github.com>
2021-02-16 17:03:34 +0300

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApp3
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(106, 117);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(107, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Введите значение х";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(106, 199);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(186, 13);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "Результат выполнения программы";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(219, 117);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(178, 20);
+      this.textBox1.TabIndex = 2;
+      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(109, 216);
+      this.textBox2.Multiline = true;
+      this.textBox2.Name = "textBox2";
+      this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.textBox2.Size = new System.Drawing.Size(288, 222);
+      this.textBox2.TabIndex = 3;
+      this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(464, 216);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 4;
+      this.button1.Text = "Рассчитать";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      textBox1.Text = "2";
+      textBox2.Text = "Практическая работа № 1 Иванов И.И.";
+      textBox2.Text += Environment.NewLine + "Рассчитать значение выражений y=формула №6";
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    private void textBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      double x = double.Parse(textBox1.Text);
+      textBox2.Text += Environment.NewLine + "При х = " + x.ToString();
+      double y = ((Math.Sqrt(Math.Pow(x, 3))-Math.Log(x)) / (Math.Cos(2*Math.Pow(x, 2)) + Math.Abs(x-3))) + ((3*Math.Pow((x-2), 2))/(Math.Log(x)+2));
+      textBox2.Text += Environment.NewLine + "Результат y = " + y.ToString();
+    }
+  }
+}
