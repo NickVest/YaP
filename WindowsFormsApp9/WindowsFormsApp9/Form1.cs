f9ec8a785e9cb11fdd63c536418e9a638f62cc49
NickVest <47671600+NickVest@users.noreply.github.com>
2021-03-09 12:15:20 +0300

    Add files via upload

---
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      dataGridView1.RowCount = 15; 
+      dataGridView1.ColumnCount = 15;
+      
+      int[,] a = new int[15, 15]; 
+      int i, j;
+      Random rand = new Random();
+
+      for (i = 0; i < 15; i++)
+        for (j = 0; j < 15; j++)
+          a[i, j] = rand.Next(-100, 100);
+
+      for (i = 0; i < 15; i++)
+        for (j = 0; j < 15; j++)
+          dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(a[i, j]);
+
+      /*int m = int.MinValue;
+      for (i = 0; i < 15; i++)
+        if (a[i, 14 - i] > m) m = a[i, 14 - i];
+      textBox1.Text = Convert.ToString(m);*/
+
+      int sum = 0;
+      for (i = 0; i < 15; i++)
+        for (j = 15 - i; j < 15; j++)
+          sum = sum + a[i, j];
+      textBox1.Text = Convert.ToString(sum);
+    }
+  }
+}
